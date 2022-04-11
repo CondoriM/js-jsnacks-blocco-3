1d8b469b19ed5f9a703cf495fdec1a40c2f93fbf
CondoriM <condoria.miguel@gmail.com>
2022-04-11 12:35:01 +0200

    cars

---
+++ snack1/assets/main.js
@@ -0,0 +1,52 @@
+const auto = [
+    {
+        marca: 'Audio',
+        modello: '2015',
+        alimentazione: 'benzina'
+    },
+    {
+        marca: 'Audio',
+        modello: '2015',
+        alimentazione: 'metano'
+    },
+    {
+        marca: 'Audio',
+        modello: '2015',
+        alimentazione: 'disel'
+    },
+    {
+        marca: 'ford',
+        modello: '2020',
+        alimentazione: 'gpl'
+    },
+    {
+        marca: 'Audio',
+        modello: '2015',
+        alimentazione: 'elettrico'
+    },
+    {
+        marca: 'lancia',
+        modello: '2012',
+        alimentazione: 'benzina'
+    },
+    {
+        marca: 'Audio',
+        modello: '2015',
+        alimentazione: 'metano'
+    },
+    {
+        marca: 'maserati',
+        modello: '2020',
+        alimentazione: 'disel'
+    },
+    {
+        marca: 'nissan',
+        modello: '2015',
+        alimentazione: 'gpl'
+    },
+    {
+        marca: 'lancia',
+        modello: '2015',
+        alimentazione: 'elettrico'
+    }
+]
